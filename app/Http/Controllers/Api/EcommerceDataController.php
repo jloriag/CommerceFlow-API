63a9b55642f3799c7c520cb1485dafe5790f63a7
Josue Loria <jloriag@hotmail.com>
2024-12-30 08:59:26 -0600

    Remove bad line controller

--- app/Http/Controllers/Api/EcommerceDataController.php
+++ app/Http/Controllers/Api/EcommerceDataController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\EcommerceData;
 use App\Http\Resources\EcommerceDataResource;
 use Illuminate\Http\Request;
