c1860fa5b8fd3e58e95a5e278e3062b17628d4f9
Josue Loria <jloriag@hotmail.com>
2024-12-30 14:11:34 -0600

    Chances

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,15 @@
 <?php
-
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\SellController;
 use App\Http\Controllers\Api\EcommerceDataController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('sells', [SellController::class, 'index']);
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to the API']);
+});
 
-Route::apiResource('products',ProductController::class);
-
-Route::apiResource('sells',SellController::class);
-
-Route::apiResource('ecommercedata',EcommerceDataController::class);
-
-Route::apiResource('user',UserController::class);
+Route::apiResource('products', ProductController::class);
+Route::apiResource('sells', SellController::class);
+Route::apiResource('ecommercedata', EcommerceDataController::class);
+Route::apiResource('user', UserController::class);
