c895439c7ba36f0a3d0a42ea731cb18f5f9dfe94
Josue Loria <jloriag@hotmail.com>
2024-12-30 10:00:59 -0600

    welcome

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
