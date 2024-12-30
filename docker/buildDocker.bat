1207f25e1508799dafc9acefeb61a0db10211a1f
Josue Loria <jloriag@hotmail.com>
2024-12-30 08:53:26 -0600

    Remove column and docker file editions

--- docker/buildDocker.bat
+++ docker/buildDocker.bat
@@ -1,4 +1,7 @@
-docker build -t ubuntu-laravel-lamp .
+docker build --no-cache -t ubuntu-laravel-lamp .
 docker rm -f laravel-container
 docker run -d -p 8081:80 -p 3307:3306 --name laravel-container ubuntu-laravel-lamp
+timeout /t 5
+docker exec -it laravel-container bash -c "mysql -u root -proot < docker/mysql-init.sql"
+docker exec -it laravel-container bash -c "php artisan migrate --force"
 docker exec -it laravel-container bash
